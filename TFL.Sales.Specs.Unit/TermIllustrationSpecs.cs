8120187434067dbddabb8b85a30b35cb2da07bf8
Alex Davis <alex.davis@fbitn.com>
2019-08-01 10:16:09 -0500

    adding additional test for correct rows per age

--- TFL.Sales.Specs.Unit/TermIllustrationSpecs.cs
+++ TFL.Sales.Specs.Unit/TermIllustrationSpecs.cs
@@ -6,9 +6,7 @@ using Xunit.Extensions;
 
 namespace TFL.Sales.Specs.Unit
 {
-    //TODO refactor so that either specification is more specific(1 verification per spec) or move executuion logic
-    //into override and makes observations more focused
-    public class illustration_should_run_until_endowment_age : Specification
+    public class illustration_should_have_annual_value_for_each_year_remaining_until_maturity : Specification
     {
         private ITermIllustrationBuilder _illustrationBuilder;
 
@@ -20,17 +18,30 @@ namespace TFL.Sales.Specs.Unit
         {
             _illustrationBuilder = new TermIllustrationBuilder();
             _insuredGender = "m";
-            _insuredAgeForIllustration = new InsuredAge(41);
+
             _product = new TermLifeProduct();
         }
 
-        [Observation(DisplayName = "illustration for 41 year old should have 55 rows")]
-        public void illustration_for_41_year_old_should_have_54_rows()
+        [Observation(DisplayName = "illustration for 41 year old should have 54 rows for annual value")]
+        public void illustration_for_41_year_old_should_have_54_rows_for_annual_values()
         {
+            _insuredAgeForIllustration = new InsuredAge(41);
+            
             var illustration = _illustrationBuilder.GenerateIllustration(new ProductIllustrationOptions<TermLifeProduct>(_product,_insuredAgeForIllustration.Value,_insuredGender));
             illustration.Values.Count.ShouldEqual(1);
             illustration.Count(x => x.Key == ProductIllustrationAnnualValuesType.CurrentValues).ShouldEqual(1);
             illustration[ProductIllustrationAnnualValuesType.CurrentValues].Count.ShouldEqual(54);
+        }
+        
+        [Observation(DisplayName = "illustration for 31 year old should have 64 rows for annual value")]
+        public void illustration_for_31_year_old_should_have_54_rows_for_annual_values()
+        {
+            _insuredAgeForIllustration = new InsuredAge(31);
+            
+            var illustration = _illustrationBuilder.GenerateIllustration(new ProductIllustrationOptions<TermLifeProduct>(_product,_insuredAgeForIllustration.Value,_insuredGender));
+            illustration.Values.Count.ShouldEqual(1);
+            illustration.Count(x => x.Key == ProductIllustrationAnnualValuesType.CurrentValues).ShouldEqual(1);
+            illustration[ProductIllustrationAnnualValuesType.CurrentValues].Count.ShouldEqual(64);
         }
     }
 }
